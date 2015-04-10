c57b2629fed62b22345511005950702422c61cff
Kris Leech <kris.leech@gmail.com>
2015-04-10 14:04:23 +0100

    move high low dice game to its own ns

--- src/blackjack/core.cljs
+++ src/blackjack/core.cljs
@@ -3,8 +3,8 @@
   (:require [reagent.core :as r]
             [secretary.core :as secretary :refer-macros [defroute]]
             [goog.events :as events]
-            [blackjack.ui :as ui]
             [blackjack.lobby :as lobby]
+            [blackjack.high-low :as high-low]
             [goog.history.EventType :as EventType])
   (:import goog.History))
 
@@ -15,86 +15,6 @@
 
 ;; Queries
 
-(defn won? []
-  (let [now  (:dice @app-state)
-        then (:previous_dice @app-state)
-        bet  (:bet @app-state)]
-    (or (and (> now then) (= bet "higher")) (and (< now then) (= bet "lower")))))
-
-;; Actions
-
-(defn calc-winner []
-    (if (won?)
-      (do 
-        (swap! app-state assoc :winner "You") 
-        (swap! app-state assoc :points (+ (:points @app-state) 10)))
-      (do 
-        (swap! app-state assoc :winner "Me")
-        (swap! app-state assoc :points (- (:points @app-state) 10)))))
-
-(defn roll-dice []
-  (let [new_number (+ (rand-int 6) 1)]
-    (swap! app-state assoc :previous_dice (:dice @app-state)) ; can we make two swaps atomic?
-    (swap! app-state assoc :dice new_number)))
-
-(defn bet-higher [] 
-  (swap! app-state assoc :bet "higher")
-  (roll-dice)
-  (calc-winner))
-
-(defn bet-lower []
-  (swap! app-state assoc :bet "lower")
-  (roll-dice)
-  (calc-winner))
-
-;; components
-
-;; DOMAIN
-
-(def dice-defaults { :width "100px"
-                     :tag   :div })
-
-(defn dice []
-  (let [this   (r/current-component)
-        props  (merge dice-defaults (r/props this))
-        number (:number props)
-        tag    (:tag    props)
-        width  (:width  props)]
-
-  [tag [:image { :src (str "images/die" number ".svg") :style { :width width } :alt number }]]))
-
-(defn bet []
-  (let [bet       (:bet           @app-state)
-        dice_then (:previous_dice @app-state)]
-
-  [:div.bet (if (not (nil? bet))
-    [:div (str "Bet: " bet " than ")
-      [dice { :tag :span :width "20px" :number dice_then }]])]))
-
-(defn player [] (let
-                  [name      (:name @app-state)
-                   points    (:points @app-state)
-                   winner    (:winner @app-state)
-                   dice_then (:previous_dice @app-state)
-                   dice_now  (:dice @app-state)]
-
-                  [:div.player
-                    [:div.name (str "Name: " name)]
-                    [:div.points (str "Points: " points)]
-                    [bet]
-                    [:div.winner (if (not (nil? winner)) (str "Winner: " winner))]
-                    [dice { :number dice_now }]
-                    [ui/button {:label "Higher" :on-click #(bet-higher)}]
-                    [ui/button {:label "Lower" :on-click #(bet-lower)}]]))
-
-(defn header []
-  [:div.header 
-    [:a { :href "/#" } "Lobby"]
-    [:a { :href "/#high-low-game" } "High Low"]])
-
-;; PAGES
-
-(defn high-low-game [] [:div [header] [player]])
 
 ;; Routes
 
@@ -105,7 +25,7 @@
   (render-page lobby/page))
 
 (defroute "/high-low-game" []
-  (render-page high-low-game))
+  (render-page high-low/page))
 
 ;; Dispatch routes based on URL
 (let [h (History.)]
@@ -115,7 +35,7 @@
 
 ;; APP
 (defn init! [] (
-  (roll-dice)
+  (high-low/roll-dice) ; move to initialize fn
   (render-page lobby/page)))
 
 (init!)
---
+++ src/blackjack/high_low.cljs
@@ -0,0 +1,84 @@
+(ns blackjack.high-low
+  (:require [reagent.core :as r]
+            [blackjack.ui :as ui]))
+
+(defn won? []
+  (let [now  (:dice @blackjack.core/app-state)
+        then (:previous_dice @blackjack.core/app-state)
+        bet  (:bet @blackjack.core/app-state)]
+    (or (and (> now then) (= bet "higher")) (and (< now then) (= bet "lower")))))
+
+;; Actions
+
+(defn calc-winner []
+    (if (won?)
+      (do 
+        (swap! blackjack.core/app-state assoc :winner "You") 
+        (swap! blackjack.core/app-state assoc :points (+ (:points @blackjack.core/app-state) 10)))
+      (do 
+        (swap! blackjack.core/app-state assoc :winner "Me")
+        (swap! blackjack.core/app-state assoc :points (- (:points @blackjack.core/app-state) 10)))))
+
+(defn roll-dice []
+  (let [new_number (+ (rand-int 6) 1)]
+    (swap! blackjack.core/app-state assoc :previous_dice (:dice @blackjack.core/app-state)) ; can we make two swaps atomic?
+    (swap! blackjack.core/app-state assoc :dice new_number)))
+
+(defn bet-higher [] 
+  (swap! blackjack.core/app-state assoc :bet "higher")
+  (roll-dice)
+  (calc-winner))
+
+(defn bet-lower []
+  (swap! blackjack.core/app-state assoc :bet "lower")
+  (roll-dice)
+  (calc-winner))
+
+;; components
+
+;; DOMAIN
+
+(def dice-defaults { :width "100px"
+                     :tag   :div })
+
+(defn dice []
+  (let [this   (r/current-component)
+        props  (merge dice-defaults (r/props this))
+        number (:number props)
+        tag    (:tag    props)
+        width  (:width  props)]
+
+  [tag [:image { :src (str "images/die" number ".svg") :style { :width width } :alt number }]]))
+
+(defn bet []
+  (let [bet       (:bet           @blackjack.core/app-state)
+        dice_then (:previous_dice @blackjack.core/app-state)]
+
+  [:div.bet (if (not (nil? bet))
+    [:div (str "Bet: " bet " than ")
+      [dice { :tag :span :width "20px" :number dice_then }]])]))
+
+(defn player [] (let
+                  [name      (:name @blackjack.core/app-state)
+                   points    (:points @blackjack.core/app-state)
+                   winner    (:winner @blackjack.core/app-state)
+                   dice_then (:previous_dice @blackjack.core/app-state)
+                   dice_now  (:dice @blackjack.core/app-state)]
+
+                  [:div.player
+                    [:div.name (str "Name: " name)]
+                    [:div.points (str "Points: " points)]
+                    [bet]
+                    [:div.winner (if (not (nil? winner)) (str "Winner: " winner))]
+                    [dice { :number dice_now }]
+                    [ui/button {:label "Higher" :on-click #(bet-higher)}]
+                    [ui/button {:label "Lower" :on-click #(bet-lower)}]]))
+
+(defn header []
+  [:div.header 
+    [:a { :href "/#" } "Lobby"]
+    [:a { :href "/#high-low-game" } "High Low"]])
+
+;; PAGES
+
+(defn page [] [:div [header] [player]])
